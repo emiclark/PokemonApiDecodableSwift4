83e76de47cdd00d43816dd55dc134437e819d581
Emi Clark <emiko.clark@gmail.com>
2018-03-19 22:54:19 -0400

    condense code into view controller

--- PokemonApiDecodableSwift4/ViewController.swift
+++ PokemonApiDecodableSwift4/ViewController.swift
@@ -8,10 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Pokemon: Decodable {
+    var name: String?
+    var url: String?
+}
 
+class ViewController: UIViewController {
+    
+    var pokemonArray = [Pokemon]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getPokemonData()
 
     }
 
@@ -21,19 +29,27 @@ class ViewController: UIViewController {
         let apiString = "http://pokeapi.co/api/v2/pokemon/"
         
         guard let apiURL = URL(string: apiString) else { print("url conversion failed"); return }
-        let request = URLRequest(url: apiURL)
         
-        URLSession.shared.dataTask(with: request) { (data, response, error) in
+        URLSession.shared.dataTask(with: apiURL) { (data, response, error) in
             guard let data = data else {print("data nil"); return }
             
             do {
-                let resultsArray = try JSONDecoder().decode([PokemonArr].self, from: data)
+                self.pokemonArray = try JSONDecoder().decode([Pokemon].self, from: data)
+                self.printPokemonArray()
+
             } catch {
                 print("error getting Pokemon data objects")
             }
         }.resume()
+        DispatchQueue.main.async() {
+            self.printPokemonArray()
+        }
     }
     
-
+    func printPokemonArray() {
+        for item in pokemonArray {
+            print(item.name as String!, item.url as String!)
+        }
+    }
 }
 
