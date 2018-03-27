b134aa7a06c21805311b4cf491e16409047a71d6
Emi Clark <emiko.clark@gmail.com>
2018-03-26 20:20:04 -0400

    created [Pokemon] using Decodable

--- PokemonApiDecodableSwift4/ViewController.swift
+++ PokemonApiDecodableSwift4/ViewController.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+class MainJSON: Decodable {
+//    var results: ResultsJSON?
+    var results: [Pokemon]?
+    var next: String?
+}
+
 struct Pokemon: Decodable {
     var name: String?
     var url: String?
@@ -34,11 +40,16 @@ class ViewController: UIViewController {
             guard let data = data else {print("data nil"); return }
             
             do {
-                self.pokemonArray = try JSONDecoder().decode([Pokemon].self, from: data)
+                let mainJson = try JSONDecoder().decode(MainJSON.self, from: data)
+                let resultsArr = mainJson.results
+                
+                print("next call string: \(mainJson.next ?? "no next page")")
+                
                 // print pokemon array
-                for item in self.pokemonArray {
+                for item in resultsArr! {
                     print(item.name as String!, item.url as String!)
                 }
+                
                 DispatchQueue.main.async {
 
                     completion()
@@ -49,4 +60,93 @@ class ViewController: UIViewController {
         }.resume()
     }
 }
+
+// json
+//{
+//    "count": 949,
+//    "previous": null,
+//    "results": [
+//    {
+//    "url": "https://pokeapi.co/api/v2/pokemon/1/",
+//    "name": "bulbasaur"
+//    },
+//    {
+//    "url": "https://pokeapi.co/api/v2/pokemon/2/",
+//    "name": "ivysaur"
+//    },
+//    {
+//    "url": "https://pokeapi.co/api/v2/pokemon/3/",
+//    "name": "venusaur"
+//    },
+//    {
+//    "url": "https://pokeapi.co/api/v2/pokemon/4/",
+//    "name": "charmander"
+//    },
+//    {
+//    "url": "https://pokeapi.co/api/v2/pokemon/5/",
+//    "name": "charmeleon"
+//    },
+//    {
+//    "url": "https://pokeapi.co/api/v2/pokemon/6/",
+//    "name": "charizard"
+//    },
+//    {
+//    "url": "https://pokeapi.co/api/v2/pokemon/7/",
+//    "name": "squirtle"
+//    },
+//    {
+//    "url": "https://pokeapi.co/api/v2/pokemon/8/",
+//    "name": "wartortle"
+//    },
+//    {
+//    "url": "https://pokeapi.co/api/v2/pokemon/9/",
+//    "name": "blastoise"
+//    },
+//    {
+//    "url": "https://pokeapi.co/api/v2/pokemon/10/",
+//    "name": "caterpie"
+//    },
+//    {
+//    "url": "https://pokeapi.co/api/v2/pokemon/11/",
+//    "name": "metapod"
+//    },
+//    {
+//    "url": "https://pokeapi.co/api/v2/pokemon/12/",
+//    "name": "butterfree"
+//    },
+//    {
+//    "url": "https://pokeapi.co/api/v2/pokemon/13/",
+//    "name": "weedle"
+//    },
+//    {
+//    "url": "https://pokeapi.co/api/v2/pokemon/14/",
+//    "name": "kakuna"
+//    },
+//    {
+//    "url": "https://pokeapi.co/api/v2/pokemon/15/",
+//    "name": "beedrill"
+//    },
+//    {
+//    "url": "https://pokeapi.co/api/v2/pokemon/16/",
+//    "name": "pidgey"
+//    },
+//    {
+//    "url": "https://pokeapi.co/api/v2/pokemon/17/",
+//    "name": "pidgeotto"
+//    },
+//    {
+//    "url": "https://pokeapi.co/api/v2/pokemon/18/",
+//    "name": "pidgeot"
+//    },
+//    {
+//    "url": "https://pokeapi.co/api/v2/pokemon/19/",
+//    "name": "rattata"
+//    },
+//    {
+//    "url": "https://pokeapi.co/api/v2/pokemon/20/",
+//    "name": "raticate"
+//    }
+//    ],
+//    "next": "https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20"
+//}
 
